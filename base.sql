51a5f0bdb32689a86fedb320658cd83c3ebde011
Erick Tapia <ericktapia09062006@gmail.com>
2025-02-12 21:57:54 -0600

    Add files via upload

---
+++ base.sql
@@ -0,0 +1,17 @@
+create database aereopuerto;
+use aereopuerto;
+
+create table avion(asiento int,disponible boolean);
+
+insert into avion values(1,true);
+insert into avion values(2,true);
+insert into avion values(3,true);
+insert into avion values(4,true);
+insert into avion values(5,true);
+insert into avion values(6,true);
+insert into avion values(7,true);
+insert into avion values(8,true);
+insert into avion values(9,true);
+insert into avion values(10,true);
+insert into avion values(11,true);
+insert into avion values(12,true);
